8c18533de9b441f0dec2d8138a644c0b43c2fdeb
Hossein Samani <samanihossein1379@gmail.com>
2022-03-13 18:33:11 +0330

    Modal Component added and bind to store

---
+++ components/layout/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-modal v-model="modalVisibility">
+    <p class="my-4">Hello from modal!</p>
+  </b-modal>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  name: 'ModalComponent',
+  computed: {
+    ...mapState(['isModalVisible']),
+    modalVisibility: {
+      get() {
+        return this.isModalVisible
+      },
+      set(val) {
+        return this.setLoadingState(val)
+      },
+    },
+  },
+  methods: {
+    ...mapActions(['setLoadingState']),
+  },
+}
+</script>
+
+<style></style>
