234a3b7b844f3e90949a7fe5e95e43d38830fadc
Hossein Samani <samanihossein1379@gmail.com>
2022-03-13 23:39:35 +0330

    nuxt configuration done

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    ssr: false,
     title: 'arvan-challange',
     htmlAttrs: {
       lang: 'en',
@@ -14,27 +14,15 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  // css: ['~/assets/css/main.scss'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
+  buildModules: ['@nuxtjs/eslint-module'],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
-  ],
+  modules: ['bootstrap-vue/nuxt'],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
