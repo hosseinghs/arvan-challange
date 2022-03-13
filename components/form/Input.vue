f967b0dccd8f80a17f0cb7e574bd6a05dc1601a7
Hossein Samani <samanihossein1379@gmail.com>
2022-03-13 19:58:36 +0330

    input component added ( error handeling is missing from the component )

--- components/form/Input.vue
+++ components/form/Input.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>
-    <b-form-input v-bind="$attrs" v-on="$listeners" />
+  <div role="group" class="text-start">
+    <label for="input-live">{{ label }}</label>
+    <b-form-input
+      :type="type"
+      aria-describedby="input-live-help input-live-feedback"
+      v-on="$listeners"
+    />
+
+    <!-- This will only be shown if the preceding input has an invalid state -->
+    <b-form-invalid-feedback id="input-live-feedback">
+      {{ errMsg }}
+    </b-form-invalid-feedback>
   </div>
 </template>
 
 <script>
 export default {
   name: 'InputComponent',
+  props: {
+    label: {
+      type: String,
+      default: 'label',
+      required: false,
+    },
+    type: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    errMsg: {
+      type: String,
+      default: 'error message',
+      required: false,
+    },
+  },
+  computed: {},
 }
 </script>
-
-<style></style>
