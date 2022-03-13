9523d8f9434e97a921623d3c82bd23e90f18d564
Hossein Samani <samanihossein1379@gmail.com>
2022-03-13 18:47:01 +0330

    input component added

---
+++ components/form/Input.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <b-form-input v-bind="$attrs" v-on="$listeners" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputComponent',
+}
+</script>
+
+<style></style>
