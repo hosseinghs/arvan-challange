2e003b07b24fdd2e4a539801e7963573d2b19ce6
Hossein Samani <samanihossein1379@gmail.com>
2022-03-13 19:02:03 +0330

    general functions added as utility functions

---
+++ utils/general.js
@@ -0,0 +1,11 @@
+export function addToArr(arr, v) {
+  if (Array.isArray(v)) arr.push(...v)
+  else arr.push(v)
+}
+
+export function createObjWithDefaultValues(self, defaults, options) {
+  const res = Object.assign({}, defaults, options)
+  Object.keys(defaults).forEach((k) => {
+    self[k] = res[k]
+  })
+}
