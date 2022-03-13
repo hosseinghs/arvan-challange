dd52bc96b8926282226daeea28c5194b1c043295
Hossein Samani <samanihossein1379@gmail.com>
2022-03-13 19:18:28 +0330

    register layout implemeted

--- layouts/register.vue
+++ layouts/register.vue
@@ -1,5 +1,9 @@
 <template>
-  <div></div>
+  <div class="_register_layout">
+    <b-col cols="11" md="6" lg="3">
+      <Register />
+    </b-col>
+  </div>
 </template>
 
 <script>
@@ -8,4 +12,12 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+._register_layout {
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+  background-color: #fff;
+}
+</style>
