003f62b16ed3527097041d25643fce890500241c
Hossein Samani <samanihossein1379@gmail.com>
2022-03-14 22:25:35 +0330

    articles table added and edit and delete methods are in place ( not functtioning though )

--- pages/admin/articles/index.vue
+++ pages/admin/articles/index.vue
@@ -1,7 +1,42 @@
 <template>
   <b-container>
-    <h1>All Posts</h1>
-    <b-table striped hover :items="articles.articles"></b-table>
+    <h1 class="mb-4">All Posts</h1>
+    <b-table striped hover :fields="fields" :items="articles.articles">
+      /*
+      --------------------------------------------------------------------------
+      */ /* customize table celss */ /*
+      --------------------------------------------------------------------------
+      */
+      <template #cell(author)="data">
+        {{ data.item.author.username }}
+      </template>
+
+      <template #cell(tags)="data">
+        <div v-for="(tag, i) in data.item.tagList" :key="i">
+          <span>{{ tag }} ,</span>
+        </div>
+      </template>
+
+      <template #cell(description)="data">
+        {{ data.item.description }}
+      </template>
+
+      <template #cell(createdAt)="data">
+        {{ new Date(data.item.createdAt).toDateString() }}
+      </template>
+
+      <template #cell(actions)="data">
+        <b-button-group class="mx-1">
+          <b-button>...</b-button>
+          <b-dropdown right>
+            <b-dropdown-item @click.stop="editPost(data.item)"
+              >Edit</b-dropdown-item
+            >
+            <b-dropdown-item>Delete</b-dropdown-item>
+          </b-dropdown>
+        </b-button-group>
+      </template>
+    </b-table>
   </b-container>
 </template>
 
@@ -9,7 +44,34 @@
 import { mapState, mapActions } from 'vuex'
 export default {
   name: 'ArticlePage',
+  data() {
+    return {
+      fields: [
+        {
+          key: 'title',
+        },
+        {
+          value: 'author',
+          key: 'author',
+        },
+        {
+          value: 'tagList',
+          key: 'tags',
+        },
+        {
+          key: 'description',
+        },
+        {
+          key: 'createdAt',
+        },
 
+        {
+          value: ' ',
+          key: 'actions',
+        },
+      ],
+    }
+  },
   computed: {
     ...mapState('articleManagement', ['articles']),
   },
@@ -19,6 +81,14 @@ export default {
 
   methods: {
     ...mapActions('articleManagement', ['getArticles']),
+    editPost(item) {
+      if (!item) return null
+      console.log(item)
+    },
+    generateWarningConfig(item) {
+      if (!item) return null
+      console.log(item)
+    },
   },
 }
 </script>
