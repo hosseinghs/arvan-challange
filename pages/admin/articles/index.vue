f5ab382164111891a0f0c0364f9bf0e405011ff8
Hossein Samani <samanihossein1379@gmail.com>
2022-03-14 21:09:56 +0330

    article module added to store and getArticles action added and bined to article page

--- pages/admin/articles/index.vue
+++ pages/admin/articles/index.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-    <article>index</article>
-  </div>
+  <b-container>
+    <h1>All Posts</h1>
+    <b-table striped hover :items="articles.articles"></b-table>
+  </b-container>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
 export default {
   name: 'ArticlePage',
+
+  computed: {
+    ...mapState('articleManagement', ['articles']),
+  },
+  created() {
+    this.getArticles()
+  },
+
+  methods: {
+    ...mapActions('articleManagement', ['getArticles']),
+  },
 }
 </script>
 
---
+++ store/admin/-article.js
@@ -0,0 +1,26 @@
+// import { addToArr } from '~/utils/general'
+import { getArticlesApi } from '~/services/articles'
+
+export default {
+  namespaced: true,
+
+  state: () => ({
+    articles: {},
+  }),
+
+  mutations: {
+    SET_ARTICLES_LIST(state, payload) {
+      state.articles = Object.assign({}, payload)
+    },
+  },
+
+  actions: {
+    async getArticles({ commit }) {
+      async function apiCall(api) {
+        const { data, status } = await getArticlesApi(api)
+        if (status === 200) commit('SET_ARTICLES_LIST', data)
+      }
+      return await this.$apiCaller(apiCall)()
+    },
+  },
+}
