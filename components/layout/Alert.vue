5be8a6722e2b1594bb91381cb6a0f99646e09748
Hossein Samani <samanihossein1379@gmail.com>
2022-03-14 19:37:19 +0330

    alert component implemented in layout

---
+++ components/layout/Alert.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-alert fade dismissible :variant="notif.color" :show="isVisible">{{
+      notif.title
+    }}</b-alert>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  name: 'AlertCompoent',
+  computed: {
+    ...mapState('notification', ['isVisible', 'notif', 'timer']),
+  },
+  methods: {
+    ...mapActions('notification', ['']),
+  },
+}
+</script>
+
+<style></style>
