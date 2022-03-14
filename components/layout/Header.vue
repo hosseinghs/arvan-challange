98c782d4755b4bed936cd7b223dd1d9c51e7b833
Hossein Samani <samanihossein1379@gmail.com>
2022-03-14 20:24:15 +0330

    Header component added and implemented in default layout

---
+++ components/layout/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="my-header">
+    <b-navbar toggleable="lg" type="dark">
+      <b-navbar-brand href="#">Arvan Challange</b-navbar-brand>
+      <span style="color: #fff"> Welcome hossein </span>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <button class="btn">Logout</button>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+}
+</script>
+
+<style scoped>
+.my-header {
+  background-color: #373a3c;
+}
+
+button {
+  color: #5bc0de;
+  border: 1px solid;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <LayoutHeader />
     <LayoutLoading />
     <LayoutAlert />
 
