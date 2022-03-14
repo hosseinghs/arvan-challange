a086b09454527f8ef7131d73b08ef3f0255da2c5
Hossein Samani <samanihossein1379@gmail.com>
2022-03-14 20:04:40 +0330

    default layout added

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <LayoutLoading />
+    <LayoutAlert />
+
+    <div class="_default_layout">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style scoped>
+._default_layout {
+  width: 100%;
+  height: 100vh;
+}
+</style>
