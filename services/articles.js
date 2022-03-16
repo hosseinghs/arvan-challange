780683615e6a49e4fdbf3ae3e285fd17c5959cc2
Hossein Samani <samanihossein1379@gmail.com>
2022-03-17 01:45:37 +0330

    CRUD added for article services

--- services/articles.js
+++ services/articles.js
@@ -3,3 +3,15 @@ const baseURL = 'articles'
 export function getArticlesApi(api) {
   return api.get(baseURL)
 }
+
+export function createArticleApi(api, article) {
+  return api.$post(baseURL, article)
+}
+
+export function editArticleApi(api, article) {
+  return api.$put(baseURL, article)
+}
+
+export function deleteArticleApi(api, article) {
+  return api.$delete(baseURL, article)
+}
