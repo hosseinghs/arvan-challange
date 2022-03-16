6ad4600e2be2284c9f08112e58084d5690d5ebf8
Hossein Samani <samanihossein1379@gmail.com>
2022-03-17 02:29:48 +0330

    CRUD actions for article added , wating for test

--- store/admin/-article.js
+++ store/admin/-article.js
@@ -1,20 +1,38 @@
 // import { addToArr } from '~/utils/general'
-import { getArticlesApi } from '~/services/articles'
+import {
+  getArticlesApi,
+  deleteArticleApi,
+  editArticleApi,
+  createArticleApi,
+} from '~/services/articles'
+import { Article } from '~/models/article'
 
 export default {
   namespaced: true,
 
   state: () => ({
     articles: {},
+    article: new Article(),
   }),
 
   mutations: {
     SET_ARTICLES_LIST(state, payload) {
       state.articles = Object.assign({}, payload)
     },
+    SET_ARTICLE_DATA(state, { k, v }) {
+      state.article[k] = v
+    },
   },
 
   actions: {
+    setArticleData({ commit }, { k, v }) {
+      commit('SET_ARTICLE_DATA', { k, v })
+    },
+
+    /* -------------------------------------------------------------------------- */
+    /*                                  api calls                                 */
+    /* -------------------------------------------------------------------------- */
+
     async getArticles({ commit }) {
       async function apiCall(api) {
         const { data, status } = await getArticlesApi(api)
@@ -22,8 +40,28 @@ export default {
       }
       return await this.$apiCaller(apiCall)()
     },
-    async deleteArticle({ commit }, yechizi) {
-      await console.log(yechizi)
+
+    async deleteArticle({ commit }, articleId) {
+      async function apiCall(api) {
+        console.log(await deleteArticleApi(api, articleId))
+      }
+      return await this.$apiCaller(apiCall)()
+    },
+
+    async editArticle({ state, commit }) {
+      const article = state.article
+      async function apiCall(api) {
+        console.log(await editArticleApi(api, article))
+      }
+      return await this.$apiCaller(apiCall)()
+    },
+
+    async createArticle({ state, commit }) {
+      const article = state.article
+      async function apiCall(api) {
+        console.log(await createArticleApi(api, article))
+      }
+      return await this.$apiCaller(apiCall)()
     },
   },
 }
