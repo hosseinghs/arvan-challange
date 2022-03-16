602d55131da3f00629b4e22e09b2f7ead8cf40ea
Hossein Samani <samanihossein1379@gmail.com>
2022-03-17 01:05:11 +0330

    AddOrEditArticle component UI implemened a little ( still needs work to be done !! )

--- components/Article/index.vue
+++ components/Article/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <div></div>
+  <b-form @submit.prevent="log">
+    <b-row>
+      <b-col cols="12" lg="9">
+        <FormInput label="Title" />
+        <FormInput label="Description" />
+        <FormInput label="Body" />
+        <b-btn class="primary">Submit</b-btn>
+      </b-col>
+      <b-col cols="12" lg="3">
+        <FormInput label="Tags" />
+      </b-col>
+    </b-row>
+  </b-form>
 </template>
 
 <script>
 export default {
   name: 'AddOrEditArticleComponent',
+  methods: {
+    log() {
+      console.log('hi')
+    },
+  },
 }
 </script>
 
