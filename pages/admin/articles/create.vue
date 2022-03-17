cc9513b2a7219809c33cf25cc7cd9c0e42f0753a
Hossein Samani <samanihossein1379@gmail.com>
2022-03-17 21:34:54 +0330

    create or edit article page added

---
+++ pages/admin/articles/create.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-form @submit.prevent="log">
+    <b-row>
+      <b-col cols="12" lg="9">
+        <FormInput label="Title" />
+        <FormInput label="Description" />
+        <FormInput label="Body" />
+        <b-btn class="primary">Submit</b-btn>
+      </b-col>
+      <b-col cols="12" lg="3">
+        <FormInput label="Tags" />
+      </b-col>
+    </b-row>
+  </b-form>
+</template>
+
+<script>
+export default {
+  name: 'AddOrEditArticlePage',
+  methods: {
+    log() {
+      console.log('hi')
+    },
+  },
+}
+</script>
