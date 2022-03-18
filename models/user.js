3048bb7d4a17e6fc216da285c3326e2dd34e8648
Hossein Samani <samanihossein1379@gmail.com>
2022-03-18 23:21:10 +0330

    Login model needed , problem handeled diffrently

--- models/user.js
+++ models/user.js
@@ -1,19 +1,9 @@
 import { createObjWithDefaultValues } from '~/utils/general'
 
-export class RegisterData {
+export class User {
   constructor(options) {
     const defaults = {
       username: null,
-      email: null,
-      password: null,
-    }
-    createObjWithDefaultValues(this, defaults, options)
-  }
-}
-
-export class LoginData {
-  constructor(options) {
-    const defaults = {
       email: null,
       password: null,
     }
