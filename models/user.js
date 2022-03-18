eb86256eccfc5f6dab868f0f88a97d8f11e1a446
Hossein Samani <samanihossein1379@gmail.com>
2022-03-18 23:12:24 +0330

    LoginData class added and User Class changed to registerData

--- models/user.js
+++ models/user.js
@@ -1,9 +1,19 @@
 import { createObjWithDefaultValues } from '~/utils/general'
 
-export class User {
+export class RegisterData {
   constructor(options) {
     const defaults = {
       username: null,
+      email: null,
+      password: null,
+    }
+    createObjWithDefaultValues(this, defaults, options)
+  }
+}
+
+export class LoginData {
+  constructor(options) {
+    const defaults = {
       email: null,
       password: null,
     }
