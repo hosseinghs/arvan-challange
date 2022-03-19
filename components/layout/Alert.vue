933aca2db43b710b33fece0a579f1846a315b31a
Hossein Samani <samanihossein1379@gmail.com>
2022-03-19 20:23:05 +0330

    alert component fixed

--- components/layout/Alert.vue
+++ components/layout/Alert.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-col cols="12" md="5" xl="2" class="ml-auto mr-3">
+  <b-col cols="12" md="5" xl="2" class="custom-alert ml-auto mr-3">
     <b-alert fade dismissible :variant="notif.color" :show="isVisible">
       {{ notif.title }}
     </b-alert>
@@ -7,16 +7,20 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState } from 'vuex'
 export default {
   name: 'AlertCompoent',
   computed: {
     ...mapState('notification', ['isVisible', 'notif', 'timer']),
   },
-  methods: {
-    ...mapActions('notification', ['']),
-  },
 }
 </script>
 
-<style></style>
+<style scoped>
+.custom-alert {
+  position: fixed;
+  top: 15%;
+  right: 5%;
+  z-index: 50 !important;
+}
+</style>
