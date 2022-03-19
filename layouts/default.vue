1c992cce67686b5cad4ec5385b886cb40979f222
hossein <samanihossein1379@gmail.com>
2022-03-19 13:48:37 +0330

    articles entry page added , register module implemented in this page instead of default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,20 +15,12 @@
 </template>
 
 <script>
-import storeModule from '~/store/admin/-article'
 export default {
   name: 'DefaultLayout',
   middleware({ store, redirect }) {
     const userToken = localStorage.getItem('authorization')
     if (!userToken) redirect('/login')
     store.dispatch('register/setIsLoggedInState', !!userToken)
-  },
-  beforeCreate() {
-    if (!this.$store.hasModule('articleManagement'))
-      this.$store.registerModule('articleManagement', storeModule)
-  },
-  beforeDestroy() {
-    this.$store.unregisterModule('articleManagement')
   },
 }
 </script>
---
+++ pages/admin/articles.vue
@@ -0,0 +1,18 @@
+<template>
+  <nuxt-child />
+</template>
+
+<script>
+import storeModule from '~/store/admin/-article'
+
+export default {
+  name: 'ArticlesEntry',
+  beforeCreate() {
+    if (!this.$store.hasModule('articleManagement'))
+      this.$store.registerModule('articleManagement', storeModule)
+  },
+  beforeDestroy() {
+    this.$store.unregisterModule('articleManagement')
+  },
+}
+</script>
